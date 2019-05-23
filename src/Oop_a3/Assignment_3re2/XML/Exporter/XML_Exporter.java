cdc04facc947a750d81a4f85b2385c8a871e4636
Alexander <alexander_agam@hotmail.com>
2019-05-23 17:30:30 +0200

    Added Oop_Project

--- src/Oop_a3/Assignment_3re2/XML/Exporter/XML_Exporter.java
+++ src/Oop_a3/Assignment_3re2/XML/Exporter/XML_Exporter.java
@@ -10,18 +10,39 @@ public class XML_Exporter {
         Tree t =
                 new Tree<>("top", new Tree[]{
                         new Tree<>("sub1"),
-                        new Tree<>("top2",new Tree[]{
+                        new Tree<>("top2", new Tree[]{
                                 new Tree<>("sub3"),
-                                new Tree<>("sub4")
+                                new Tree<>("sub4"),
+                                new Tree<>("top2", new Tree[]{
+                                        new Tree<>("sub3", new Tree[]{
+                                                new Tree<>("sub3"),
+                                                new Tree<>("sub4"),
+                                                new Tree<>("top2", new Tree[]{
+                                                        new Tree<>("sub3"),
+                                                        new Tree<>("sub4")
+                                                }),
+                                        }),
+                                        new Tree<>("sub4")
+                                }),
                         }),
-                        new Tree<>("sub2")
+                        new Tree<>("top3", new Tree[]{
+                                new Tree<String>("sub5"),
+                                new Tree<String>("sub6"),
+                                new Tree<>("sub7")
+                        })
                 }
         );
+
+        Tree tt = new Tree<>("one");
+        @SuppressWarnings("unchecked")
+        Tree ttt = new Tree<>("one", new Tree[]{new Tree<>("1"), new Tree<>("2"), new Tree<>("3")});
 
         Saver s = new Saver();
         String r = null;
             r = s.save(t);
-        System.out.println(r + "\n");
+        System.out.println(r);
+        System.out.println("\n" + s.save(tt));
+        System.out.println("\n" + s.save(ttt));
 
         /*
         Method[] methodArr = t.getClass().getDeclaredMethods();
